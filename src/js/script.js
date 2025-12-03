027af2852f2f5f58cc7400aabe86340809621e56
ThirietIzya <izya.thiriet@edu.univ-fcomte.fr>
2025-12-03 08:35:40 +0100

    TP5

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightbox img");
+
+lightbox?.addEventListener("click", () => lightbox.close());
+
+document.body.addEventListener("click", (evt) => {
+  if (!evt.target.matches("[data-full-img]")) return;
+  lightboxImg.src = evt.target.dataset.fullImg;
+  lightbox.showModal();
+});
